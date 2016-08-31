103acf9d883f53d0fab95cb5774b55cfd4d3b1a2
Michael Clemens <mclemens@yahoo.com>
2016-08-30 18:30:44 -0700

    Random walk for scooty3

--- tk/scooty3.py
+++ tk/scooty3.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
 """Basic player control, wall/collision sensing, and goal reaching"""
 
+import random
+import tkinter
+import time
+
+ICONSIZE = 32            # pixel size for each game icon (sprite)
+TILESIZE = ICONSIZE * 2  # pixel size of the floor tiles
+SCALE = 20               # length & width of gameboard, in icon units
+
+# overall window sizing
+WIDTH = ICONSIZE * SCALE
+HEIGHT = ICONSIZE * SCALE
+
 ###
 
 class Player:
 
     import tkinter
+    import time
     
     ICONS = {}
 
@@ -13,8 +26,12 @@ class Player:
     DIRECTIONS = [tkinter.N, tkinter.NE, tkinter.E, tkinter.SE,
                   tkinter.S, tkinter.SW, tkinter.W, tkinter.NW]
     
-    def __init__(self):
+    def __init__(self, canvas):
         self.facing = tkinter.N
+        self.x = 1
+        self.y = 1
+        self.canvas = canvas
+        
         # lazy init: when the class is defined, the Tk instance is not ready yet
         if len(Player.ICONS) == 0:
             Player.ICONS[tkinter.N]  = tkinter.PhotoImage(file="./img/kelvin_n.gif")
@@ -26,6 +43,9 @@ class Player:
             Player.ICONS[tkinter.W]  = tkinter.PhotoImage(file="./img/kelvin_w.gif")
             Player.ICONS[tkinter.NW] = tkinter.PhotoImage(file="./img/kelvin_nw.gif")
 
+        self._tk_id = self.canvas.create_image(self.x*ICONSIZE, self.y*ICONSIZE,
+                                               image=Player.ICONS[self.facing],
+                                               anchor=tkinter.NW)
             
     def turn_toward(self,direction):
         """Orient the player in the given direction, taking the fewest number of moves possible"""
@@ -60,39 +80,56 @@ class Player:
                 # clockwise turn, off the high end of the array
                 self._turn_until(current_index, direction, -1)
 
-
     def _turn_until(self,current_index, goal_direction, increment):
         """Animate the turn"""
         while self.facing != goal_direction:
+            time.sleep(0.1)
             current_index = (current_index + increment) % 8
             self.facing = Player.DIRECTIONS[current_index]
-            # xxx update graphic
-            print("xxx Facing: ", self.facing)
-            
+            self.canvas.itemconfig(self._tk_id, image=Player.ICONS[self.facing])
 
-    def put_at(self, canvas, x, y):
-        """Params: (canvas, x, y)
-    
-        canvas: reference to the Tk canvas
-        x, y: game coords, which will be scaled for placement (0,0), (1,0), etc"""
+    def step_facing(self):
+        """Move one step in the direction that the player is facing"""
 
-        canvas.create_image(x*ICONSIZE, y*ICONSIZE,
-                            image=Player.ICONS[self.facing],
-                            anchor=tkinter.NW)
+        if self.facing == tkinter.N:
+            self.canvas.move(self._tk_id, 0, -ICONSIZE)
+            self.y -= 1
+        elif self.facing == tkinter.E:
+            self.canvas.move(self._tk_id, ICONSIZE, 0)
+            self.x += 1
+        elif self.facing == tkinter.W:
+            self.canvas.move(self._tk_id, -ICONSIZE, 0)
+            self.x -= 1
+        elif self.facing == tkinter.S:
+            self.canvas.move(self._tk_id, 0, ICONSIZE)
+            self.y += 1
+        else:
+            # No diagonal moves allowed
+            pass
+
+    def facing_point(self):
+        """Return the coordinates of the block the player is facing"""
+
+        if self.facing == tkinter.N:
+            return((self.x, self.y-1))
+        elif self.facing == tkinter.NE:
+            return((self.x+1, self.y-1))
+        elif self.facing == tkinter.E:
+            return((self.x+1, self.y))
+        elif self.facing == tkinter.SE:
+            return((self.x+1, self.y+1))
+        elif self.facing == tkinter.S:
+            return((self.x, self.y+1))
+        elif self.facing == tkinter.SW:
+            return((self.x-1, self.y+1))
+        elif self.facing == tkinter.W:
+            return((self.x-1, self.y))
+        elif self.facing == tkinter.NW:
+            return((self.x-1, self.y-1))
+        else:
+            pass
         
-            
 ###
-
-import random
-import tkinter
-
-ICONSIZE = 32            # pixel size for each game icon (sprite)
-TILESIZE = ICONSIZE * 2  # pixel size of the floor tiles
-SCALE = 20               # length & width of gameboard, in icon units
-
-# overall window sizing
-WIDTH = ICONSIZE * SCALE
-HEIGHT = ICONSIZE * SCALE
 
 tk = tkinter.Tk()
 world = tkinter.Canvas(tk, width=WIDTH, height=HEIGHT)
@@ -180,8 +217,27 @@ for x in range(SCALE):
 # goal in the bottom right
 put_goal(world,18,18)
         
-p = Player()
-p.put_at(world,1,1)
+p = Player(world)
+tk.update()
 
-tkinter.mainloop()
-    
+# tkinter.mainloop()
+
+game_over = False
+
+while not game_over:
+    tk.update_idletasks()
+    tk.update()
+    time.sleep(0.1)
+
+    if (p.facing_point() in goals):
+        p.step_facing()
+        tk.update()
+        game_over = True        
+    elif (p.facing_point() in barriers or
+          p.facing in [tkinter.NE, tkinter.SE, tkinter.SW, tkinter.NW]):
+        p.turn_toward(random.choice(Player.DIRECTIONS))
+    elif (random.randint(0,100) > 80):
+        p.turn_toward(random.choice(Player.DIRECTIONS))
+    else:        
+        p.step_facing()
+
