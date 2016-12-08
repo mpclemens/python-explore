dfb59281e004565eba314932ecfa4f24c13df6fd
Michael Clemens <mclemens128@gmail.com>
2016-12-08 15:33:15 -0800

    Cleanup per pylint*

--- turtle/bloom.py
+++ turtle/bloom.py
@@ -4,22 +4,28 @@ import random
 
 def bloom(radius):
     turtle.colormode(255)
-    
-    for rad in range (40, 10, -5):
-        for iter in range(360//rad):
+
+    for rad in range(40, 10, -5):
+        for looper in range(360//rad):
             turtle.up()
-            turtle.circle(radius+rad,rad)
-            turtle.begin_fill();
-            turtle.fillcolor((200+random.randint(0,rad),
-                              200+random.randint(0,rad),
-                              200+random.randint(0,rad)))
+            turtle.circle(radius+rad, rad)
+            turtle.begin_fill()
+            turtle.fillcolor((200+random.randint(0, rad),
+                              200+random.randint(0, rad),
+                              200+random.randint(0, rad)))
             turtle.down()
             turtle.circle(-rad)
-            turtle.end_fill();
+            turtle.end_fill()
 
-            
 
-turtle.speed(0)
-turtle.colormode(1.0)
-bloom(5)
-turtle.exitonclick()
+def main():
+    """Simple flower, using global turtle instance"""
+    turtle.speed(0)
+    turtle.colormode(1.0)
+    bloom(5)
+    turtle.exitonclick()
+
+###
+
+if __name__ == "__main__":
+    main()
